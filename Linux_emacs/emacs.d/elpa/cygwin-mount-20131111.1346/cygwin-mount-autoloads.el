9c57b1afe96fa2e33f17bc010fdb6be1e0279b56
Cesar Lizarraga <calizarr@gmail.com>
2016-05-15 12:26:05 -0500

    Moving Linux VirtualBox

---
+++ Linux_emacs/emacs.d/elpa/cygwin-mount-20131111.1346/cygwin-mount-autoloads.el
@@ -0,0 +1,15 @@
+;;; cygwin-mount-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("cygwin-mount.el") (22011 824 808250 693000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; cygwin-mount-autoloads.el ends here
