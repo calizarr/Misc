3822f2b594c8f9a451969fd2a0c4f8066954cb78
U-MSI\Dummy <calizarr@gmail.com>
2018-03-19 07:36:48 -0500

    Added in new .emacs and .emacs.d changes

---
+++ Emacs_Config/.emacs.d/elpa/fish-mode-0.1.4/fish-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "fish-mode" "0.1.4" "Major mode for fish shell scripts" '((emacs "24")) :commit "bac709ac1235751952d6022dddc6307d9135d096" :keywords '("fish" "shell"))
